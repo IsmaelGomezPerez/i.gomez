26ca0a362694fdb0914d225f8210f51f3c658c9c
Ismael Antonio Gomez Perez <ismaelgpay@gmail.com>
2022-03-23 20:32:19 -0600

    preba

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+const typed = new Typed('.typed1', {
+	strings: [
+		'',"smael","."
+	],
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 3000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra
+    loop: true,
+    backDelay: 1500,
+    contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+const typed2 = new Typed('.typed2', {
+	strings: [
+		'',"mael","."
+	],
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 3000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra
+    backDelay: 1500,
+    loop: true,
+    contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+const typed3 = new Typed('.typed3', {
+	strings: [
+		'.',"smael ","."
+	],
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 3000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra
+    backDelay: 1500,
+    loop: true,
+    contentType: 'html', // 'html' o 'null' para texto sin formato
+});
